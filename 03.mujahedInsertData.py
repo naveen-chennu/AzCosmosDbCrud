dda798c9e52c7cb6f3a8d0229f6b3021f105158c
naveen-chennu <94835549+naveen-chennu@users.noreply.github.com>
2021-11-23 12:39:55 +0530

    Update 03.mujahedInsertData.py

--- 03.mujahedInsertData.py
+++ 03.mujahedInsertData.py
@@ -1,8 +1,8 @@
 from azure.cosmos import CosmosClient
 import os
 
-os.environ['ACCOUNT_URI']="https://acc1031.documents.azure.com:443/"
-os.environ['ACCOUNT_KEY']="W4r57wzXqX10Dt0s8i8McSMuDDuPCsO4rxsiUi3Xj5vtzpzYO1H9VxTwoQ5M3yldjuaUGAckvLOFMXxIzm6hIQ=="
+# os.environ['ACCOUNT_URI']="https://acc1031.documents.azure.com:443/"
+# os.environ['ACCOUNT_KEY']="W4r57wzXqX10Dt0s8i8McSMuDDuPCsO4rxsiUi3Xj5vtzpzYO1H9VxTwoQ5M3yldjuaUGAckvLOFMXxIzm6hIQ=="
 
 url = os.environ['ACCOUNT_URI']
 key = os.environ['ACCOUNT_KEY']
